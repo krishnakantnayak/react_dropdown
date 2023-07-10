79c6412874022925e6ebd615279796c8cc2a7925
KRISHNA NAYAK <krishnakantn76@gmail.com>
2023-07-10 10:06:01 +0530

    ok

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,11 @@
 
 import Dropdown from './Dropdown'
 function App() {
+  //below arrays is the dropdown values array 
   const options=["Yes","Probably Not"];
   return (
     <div className="App">
+      {/*passed as props to dropdown component*/}
       <Dropdown Options={options}/>
     </div>
   );
